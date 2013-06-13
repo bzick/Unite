4f201a4218125b0d9b82412474c5d78ac0caf404
bzick <aco.best@gmail.com>
2013-06-13 19:00:31 +0400

    Fix indents

--- src/Unite.php
+++ src/Unite.php
@@ -1,216 +1,216 @@
 <?php
 
 namespace {
-	use Unite\ErrorException;
-	use Unite\ListenerInterface;
-	use Unite\Printer;
-	use Unite\Test\RootSuite;
+    use Unite\ErrorException;
+    use Unite\ListenerInterface;
+    use Unite\Printer;
+    use Unite\Test\RootSuite;
 
-	/**
-	 * Unit Testing engine
-	 */
-	class Unite {
-	    /**
-	     * @var Unite\Test\Suite[]
-	     */
-	    public $suites = array();
-	    /**
-	     * @var Unite\Test\File[]
-	     */
-	    public $files = array();
-	    /**
-	     * @var Unite\Test\TestCase[]
-	     */
-	    public $cases = array();
-	    /**
-	     * @var Unite\Test[]
-	     */
-	    public $tests = array();
-		/**
-		 * @var Unite\Test\TestCase[]
-		 */
-		public $classes = array();
-	    /**
-	     * @var Unite\Request
-	     */
-	    public $request;
+    /**
+     * Unit Testing engine
+     */
+    class Unite {
+        /**
+         * @var Unite\Test\Suite[]
+         */
+        public $suites = array();
+        /**
+         * @var Unite\Test\File[]
+         */
+        public $files = array();
+        /**
+         * @var Unite\Test\TestCase[]
+         */
+        public $cases = array();
+        /**
+         * @var Unite\Test[]
+         */
+        public $tests = array();
+        /**
+         * @var Unite\Test\TestCase[]
+         */
+        public $classes = array();
+        /**
+         * @var Unite\Request
+         */
+        public $request;
 
-	    public $printer;
+        public $printer;
 
-	    public $listeners = [];
+        public $listeners = [];
 
-	    /**
-	     * @param $request
-	     */
-	    public function __construct(Unite\Request $request) {
-	        $this->request = $request;
-		    $this->suite = new RootSuite($this);
-		    $this->setPrinter(new Printer($this));
-	        if($request->paths) {
-	            foreach($request->paths as $path) {
-	                $path = rtrim($path, DIRECTORY_SEPARATOR);
-	                if($path[0] != "/") {
-	                    $path = getcwd()."/".$path;
-	                }
-	                $this->addPath($path);
-	            }
-	        }
-		    set_error_handler(function ($errno, $error, $file, $line) {
-			    throw new ErrorException("$error in $file:$line", $errno);
-		    });
-	    }
+        /**
+         * @param $request
+         */
+        public function __construct(Unite\Request $request) {
+            $this->request = $request;
+            $this->suite = new RootSuite($this);
+            $this->setPrinter(new Printer($this));
+            if($request->paths) {
+                foreach($request->paths as $path) {
+                    $path = rtrim($path, DIRECTORY_SEPARATOR);
+                    if($path[0] != "/") {
+                        $path = getcwd()."/".$path;
+                    }
+                    $this->addPath($path);
+                }
+            }
+            set_error_handler(function ($errno, $error, $file, $line) {
+                    throw new ErrorException("$error in $file:$line", $errno);
+                });
+        }
 
-		public function setPrinter(ListenerInterface $printer) {
-			$this->printer = $printer;
-			$this->addListener($printer);
-		}
+        public function setPrinter(ListenerInterface $printer) {
+            $this->printer = $printer;
+            $this->addListener($printer);
+        }
 
-		public function addListener(ListenerInterface $printer) {
-			$this->listeners[] = $printer;
-		}
+        public function addListener(ListenerInterface $printer) {
+            $this->listeners[] = $printer;
+        }
 
-	    /**
-	     * @param $paths
-	     * @return $this
-	     */
-	    public function load($paths) {
-	        foreach((array)$paths as $path) {
-		        $this->addPath($path);
-	        }
-		    return $this;
-	    }
+        /**
+         * @param $paths
+         * @return $this
+         */
+        public function load($paths) {
+            foreach((array)$paths as $path) {
+                $this->addPath($path);
+            }
+            return $this;
+        }
 
-	    /**
-	     *
-	     * @param string $path path to tests. Glob syntax supported
-	     * @throws \InvalidArgumentException
-	     */
-	    public function addPath($path) {
-		    foreach(new \GlobIterator($path, FilesystemIterator::CURRENT_AS_PATHNAME) as $file) {
-			    /* @var \splFileInfo $file */
-			    if(is_dir($file)) {
-				    $this->suite->addSuite($file);
-			    } else {
-				    $this->suite->addFile($file);
-			    }
-		    }
-		    foreach(new RecursiveIteratorIterator($this->suite, RecursiveIteratorIterator::SELF_FIRST) as $file) {
-			    /* @var Unite\Test\File|Unite\Test\Suite $file */
-			    if($file instanceof \Unite\Test\Suite) {
-				    $file->setTestSuite($this);
-				    $this->suites[ $file->getRealPath() ] = $file;
-			    } elseif($this->isTestFile($file)) {
-				    $file->setTestFile($this);
-					$this->files[ $file->getRealPath() ] = $file;
-				    foreach($file->classes as $class) {
-					    /* @var Unite\Test\TestCase $class */
-					    $this->classes[ $class->name ] = $class;
-					    if($this->isTestCase($class)) {
-						    $class->setTestCase($this);
-						    $this->cases[ $class->name ] = $class;
-						    foreach($class->methods as $name => $method) {
-							    /* @var Unite\Test $method */
-							    if($this->isTest($method)) {
-								    $method->setTest($this);
-								    $this->tests[ $method->real_name ] = $method;
-							    } else {
-								    unset($class->methods[$name]);
-							    }
-						    }
-					    }
-				    }
-			    }
-		    }
-	    }
+        /**
+         *
+         * @param string $path path to tests. Glob syntax supported
+         * @throws \InvalidArgumentException
+         */
+        public function addPath($path) {
+            foreach(new \GlobIterator($path, FilesystemIterator::CURRENT_AS_PATHNAME) as $file) {
+                /* @var \splFileInfo $file */
+                if(is_dir($file)) {
+                    $this->suite->addSuite($file);
+                } else {
+                    $this->suite->addFile($file);
+                }
+            }
+            foreach(new RecursiveIteratorIterator($this->suite, RecursiveIteratorIterator::SELF_FIRST) as $file) {
+                /* @var Unite\Test\File|Unite\Test\Suite $file */
+                if($file instanceof \Unite\Test\Suite) {
+                    $file->setTestSuite($this);
+                    $this->suites[ $file->getRealPath() ] = $file;
+                } elseif($this->isTestFile($file)) {
+                    $file->setTestFile($this);
+                    $this->files[ $file->getRealPath() ] = $file;
+                    foreach($file->classes as $class) {
+                        /* @var Unite\Test\TestCase $class */
+                        $this->classes[ $class->name ] = $class;
+                        if($this->isTestCase($class)) {
+                            $class->setTestCase($this);
+                            $this->cases[ $class->name ] = $class;
+                            foreach($class->methods as $name => $method) {
+                                /* @var Unite\Test $method */
+                                if($this->isTest($method)) {
+                                    $method->setTest($this);
+                                    $this->tests[ $method->real_name ] = $method;
+                                } else {
+                                    unset($class->methods[$name]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-		/**
-		 * Check, is test file
-		 * @param \Unite\Test\File $file
-		 * @return bool
-		 */
-		public function isTestFile(Unite\Test\File $file) {
-			return (bool)stripos($file->getBasename(), "test.php");
-		}
+        /**
+         * Check, is test file
+         * @param \Unite\Test\File $file
+         * @return bool
+         */
+        public function isTestFile(Unite\Test\File $file) {
+            return (bool)stripos($file->getBasename(), "test.php");
+        }
 
-		/**
-		 * Check, is test case
-		 * @param Unite\Test\TestCase $class
-		 * @return bool
-		 */
-		public function isTestCase(Unite\Test\TestCase $class) {
-			return (bool)stripos($class->name, "test");
-		}
+        /**
+         * Check, is test case
+         * @param Unite\Test\TestCase $class
+         * @return bool
+         */
+        public function isTestCase(Unite\Test\TestCase $class) {
+            return (bool)stripos($class->name, "test");
+        }
 
-		/**
-		 * Check, is test
-		 * @param Unite\Test $test
-		 * @return bool
-		 */
-		public function isTest(Unite\Test $test) {
-			return stripos($test->name, "test") === 0 || $test->hasParam("test");
-		}
+        /**
+         * Check, is test
+         * @param Unite\Test $test
+         * @return bool
+         */
+        public function isTest(Unite\Test $test) {
+            return stripos($test->name, "test") === 0 || $test->hasParam("test");
+        }
 
-		private function _trigger($event, $arg) {
-			foreach($this->listeners as $listener) {
-				try {
-					$listener->$event($arg);
-				} catch(\Exception $e) {
-					var_dump("Listener error $e");
-				}
-			}
-		}
+        private function _trigger($event, $arg) {
+            foreach($this->listeners as $listener) {
+                try {
+                    $listener->$event($arg);
+                } catch(\Exception $e) {
+                    var_dump("Listener error $e");
+                }
+            }
+        }
 
-		/**
-		 * Run tests
-		 */
-		public function run() {
-			$this->_trigger("begin", $this);
-			$prev = current($this->tests);
-			/* @var \Unite\Test $prev */
-			$this->_trigger("beginTestSuite", $prev->case->file->suite);
-			$prev->case->file->suite->before();
-			$this->_trigger("beginTestCase", $prev->case);
-			$prev->case->before();
-			foreach($this->tests as $test) {
-				if($prev->case->suite !== $test->case->suite) { // test suite changed
-					$this->_trigger("endTestSuite", $prev->case->suite);
-					$prev->case->suite->after();
-					if(!$test->case->suite->started) {
-						$this->_trigger("beginTestSuite", $test->case->suite);
-						$test->case->suite->before();
-					}
-				}
-				if($prev->case !== $test->case) { // test case changed
-					$this->_trigger("endTestCase", $prev->case);
-					$prev->case->after();
-					$this->_trigger("beginTestCase", $test->case);
-					$test->case->before();
-				}
-				try {
-					$this->_trigger("beginTest", $test);
-					$test->before();
-					$test->run([]);
-					$this->_trigger("endTest", $test);
-					$test->success();
-				} catch(\Exception $e) {
-					$test->resolve($e);
-				}
-				$test->after();
-				$prev = $test;
-			}
-			$this->_trigger("endTestCase", $prev->case);
-			$prev->case->after();
-			$this->_trigger("endTestSuite", $prev->case->file->suite);
-			$prev->case->file->suite->after();
-		}
-	}
+        /**
+         * Run tests
+         */
+        public function run() {
+            $this->_trigger("begin", $this);
+            $prev = current($this->tests);
+            /* @var \Unite\Test $prev */
+            $this->_trigger("beginTestSuite", $prev->case->file->suite);
+            $prev->case->file->suite->before();
+            $this->_trigger("beginTestCase", $prev->case);
+            $prev->case->before();
+            foreach($this->tests as $test) {
+                if($prev->case->suite !== $test->case->suite) { // test suite changed
+                    $this->_trigger("endTestSuite", $prev->case->suite);
+                    $prev->case->suite->after();
+                    if(!$test->case->suite->started) {
+                        $this->_trigger("beginTestSuite", $test->case->suite);
+                        $test->case->suite->before();
+                    }
+                }
+                if($prev->case !== $test->case) { // test case changed
+                    $this->_trigger("endTestCase", $prev->case);
+                    $prev->case->after();
+                    $this->_trigger("beginTestCase", $test->case);
+                    $test->case->before();
+                }
+                try {
+                    $this->_trigger("beginTest", $test);
+                    $test->before();
+                    $test->run([]);
+                    $this->_trigger("endTest", $test);
+                    $test->success();
+                } catch(\Exception $e) {
+                    $test->resolve($e);
+                }
+                $test->after();
+                $prev = $test;
+            }
+            $this->_trigger("endTestCase", $prev->case);
+            $prev->case->after();
+            $this->_trigger("endTestSuite", $prev->case->file->suite);
+            $prev->case->file->suite->after();
+        }
+    }
 }
 
 namespace Unite {
-	class FailureException extends \Exception {}
-	class AssertFailureException extends FailureException {}
-	class SuccessException extends \Exception {}
-	class SkipException extends \Exception {}
-	class SkipCaseException extends SkipException {}
-	class ErrorException extends \Exception {}
+    class FailureException extends \Exception {}
+    class AssertFailureException extends FailureException {}
+    class SuccessException extends \Exception {}
+    class SkipException extends \Exception {}
+    class SkipCaseException extends SkipException {}
+    class ErrorException extends \Exception {}
 }
--- src/Unite/Test/TestCase.php
+++ src/Unite/Test/TestCase.php
@@ -5,99 +5,99 @@ use Unite\ParserTrait;
 use Unite\Test;
 
 class TestCase extends \ReflectionClass {
-	use ParserTrait;
-	/**
-	 * @var File
-	 */
-	public $file;
-	/**
-	 * @var Suite
-	 */
-	public $suite;
-	/**
-	 * @var mixed
-	 */
-	public $object;
-	/**
-	 * @var \Unite\Test[]
-	 */
-	public $methods = [];
+    use ParserTrait;
+    /**
+     * @var File
+     */
+    public $file;
+    /**
+     * @var Suite
+     */
+    public $suite;
+    /**
+     * @var mixed
+     */
+    public $object;
+    /**
+     * @var \Unite\Test[]
+     */
+    public $methods = [];
 
-	/**
-	 * @var \ReflectionMethod
-	 */
-	public $before;
+    /**
+     * @var \ReflectionMethod
+     */
+    public $before;
 
-	/**
-	 * @var \ReflectionMethod
-	 */
+    /**
+     * @var \ReflectionMethod
+     */
     public $after;
 
-	/**
-	 * @var \ReflectionMethod
-	 */
-	public $before_test;
+    /**
+     * @var \ReflectionMethod
+     */
+    public $before_test;
 
-	/**
-	 * @var \ReflectionMethod
-	 */
-	public $after_test;
+    /**
+     * @var \ReflectionMethod
+     */
+    public $after_test;
 
-	/**
-	 * @var \Unite
-	 */
-	public $unite;
+    /**
+     * @var \Unite
+     */
+    public $unite;
 
-	public $start_time = 0.0;
+    public $start_time = 0.0;
 
-	public $time = 0.0;
+    public $time = 0.0;
 
-	public function __construct($class, File $file) {
-		$this->file = $file;
-		$this->suite = $file->suite;
-		parent::__construct($class);
-		$this->_parseComments();
-	}
-
-	/**
-	 *
-	 */
-	public function setTestCase() {
-		$this->object = $this->newInstance();
-	    foreach($this->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
-		    $this->methods[$method->name] = $test = new Test($this, $method->name);
-	    }
-	    if(isset($this->methods["before"])) {
-		    $this->before = $this->methods["before"];
-	    }
-	    if(isset($this->methods["after"])) {
-		    $this->after = $this->methods["after"];
-	    }
-	    if(isset($this->methods["beforeTest"])) {
-		    $this->before_test = $this->methods["beforeTest"];
-	    }
-	    if(isset($this->methods["afterTest"])) {
-		    $this->after_test = $this->methods["afterTest"];
-	    }
+    public function __construct($class, File $file) {
+        $this->file = $file;
+        $this->suite = $file->suite;
+        parent::__construct($class);
+        $this->_parseComments();
     }
 
-	/**
-	 *
-	 */
-	public function before() {
-		if($this->before) {
-			$this->before->invoke($this->object);
-		}
-		$this->start_time = microtime(true);
+    /**
+     *
+     */
+    public function setTestCase() {
+        $this->object = $this->newInstance();
+        foreach($this->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
+            $this->methods[$method->name] = $test = new Test($this, $method->name);
+        }
+        if(isset($this->methods["before"])) {
+            $this->before = $this->methods["before"];
+        }
+        if(isset($this->methods["after"])) {
+            $this->after = $this->methods["after"];
+        }
+        if(isset($this->methods["beforeTest"])) {
+            $this->before_test = $this->methods["beforeTest"];
+        }
+        if(isset($this->methods["afterTest"])) {
+            $this->after_test = $this->methods["afterTest"];
+        }
     }
 
-	/**
-	 *
-	 */
-	public function after() {
-		$this->total_time = microtime(true) - $this->start_time;
-	    if($this->after) {
-		    $this->after->invoke($this->object);
-	    }
+    /**
+     *
+     */
+    public function before() {
+        if($this->before) {
+            $this->before->invoke($this->object);
+        }
+        $this->start_time = microtime(true);
+    }
+
+    /**
+     *
+     */
+    public function after() {
+        $this->total_time = microtime(true) - $this->start_time;
+        if($this->after) {
+            $this->after->invoke($this->object);
+        }
     }
 }
